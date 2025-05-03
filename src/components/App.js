9514b141f97a0902dd3eb554b6f4d42c36b128df
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-03 12:57:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
+import AdminPanel from './components/AdminPanel';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
+    <Router>
+      <div>
         {/* Do not remove the main div */}
-    </div>
-  )
+        <h1>Online Mobile Store</h1>
+        <nav>
+          <a href="/">Home</a> | <a href="/admin">Admin Panel</a>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
